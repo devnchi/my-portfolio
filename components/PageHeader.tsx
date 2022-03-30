0ff0ef3c9f084710e82e6fd8f210de67cc6194c4
Chi Igbokwe <ch11gbokwedevelopment@gmail.com>
2022-03-30 18:07:31 -0400

    styling

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -10,11 +10,11 @@ function PageHeader({pageTitle, projectDescription, projectTech}: Props) {
   return (
     <div className='items-center justify-center p-20 text-center mx-auto md:my-20 md:p-40'>
         <div>
-            <h3 className='text-9xl font-comforterBrush'>{pageTitle}</h3>
+            <h3 className='text-7xl md:text-9xl font-comforterBrush'>{pageTitle}</h3>
         </div>
         <div className='pt-4 p-4'>
           <div>
-            <p className='text-xl font-spaceMono text-center p-2'>{projectDescription}</p>
+            <p className='text-sm md:text-xl font-spaceMono text-center p-2'>{projectDescription}</p>
           </div>
           <div className='pt-4 text-center justify-center'>
             <span className='text-3xl font-comforterBrush font-bold text-purple-600'>{projectTech}</span>
