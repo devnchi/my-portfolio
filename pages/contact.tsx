d9bf1ea38fedd99c43db2b424960cb1d2e5b8a8a
Chi Igbokwe <ch11gbokwedevelopment@gmail.com>
2022-05-31 10:29:27 -0400

    contact page edit

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -22,7 +22,7 @@ function Contact({}: Props) {
         projectTech=''
       />
       <div className='bg-black min-h-screen relative lg:flex'>
-        <div className='mx-auto max-w-7xl text-white text-4xl font-spaceMono font-bold py-16 mb-5 w-1/2 p-2 h-1/2 lg:mt-72 lg:p-20 lg:text-5xl min-h-max'>
+        <div className='text-center mx-auto max-w-7xl text-white text-4xl font-spaceMono font-bold py-16 mb-5 w-1/2 p-2 h-1/2 lg:mt-72 lg:p-20 lg:text-5xl min-h-max'>
           <Typewriter
             onInit={(typewriter)=> {       
               typewriter          
@@ -32,8 +32,11 @@ function Contact({}: Props) {
                 .start();
               }}
             />
+            <div className='p-4 m-4'>
+              <a target='_blank' href="mailto:chiigbokwedev@gmail.com"><button className='text-white bg-purple-500 p-5 text-sm rounded-xl'>Send Chi A Message</button></a>
+            </div>
         </div>
-        <div className='p-6 align-center font-comforterBrush relative block container items-center text-center justify-center lg:flex lg:text-justify lg:justify-evenly lg:max-w-8xl md:mx-auto lg:mt-0 lg:w-1/2'>
+        {/* <div className='p-6 align-center font-comforterBrush relative block container items-center text-center justify-center lg:flex lg:text-justify lg:justify-evenly lg:max-w-8xl md:mx-auto lg:mt-0 lg:w-1/2'>
           <form name='contact' method='POST' data-netlify='true' className='text-center border-2 p-5 items-center justify-center mx-auto'>
             <input type='hidden' name='for-name' value='contact'/>
             <p className='p-2'>
@@ -52,7 +55,7 @@ function Contact({}: Props) {
               <button className='bg-purple-400 rounded-xl text-3xl text-white font-comforterBrush items-center justify-center p-5 hover:bg-gray-400 cursor-pointer' type='submit'>Send</button>
             </p>
           </form>
-        </div>
+        </div> */}
       </div>
       <Footer />
     </div>
